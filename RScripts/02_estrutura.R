6b527f7119ee7231f7e5b7f0f0ddc0a7c366d9e5
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-28 01:29:47 -0300

    aula 02 criada

---
+++ RScripts/02_estrutura.R
@@ -0,0 +1,17 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'process_covariate_balance.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'process_covariate_patient17.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', out.height='80%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'electoral-causality-loop.jpg'))
+
+
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "01_um_ensaio_aleatorizado"
+arquivo <- "02_estrutura"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
